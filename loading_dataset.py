f7fcee324af559c598b72d8c72ca155448e65875
Simeon Ikudabo <simeonikudabo@Simeons-MBP.lan>
2023-12-06 15:37:06 -0500

    Loading the dataset

--- loading_dataset.py
+++ loading_dataset.py
@@ -3,5 +3,5 @@ from pathlib import Path
 
 base_path = Path(__file__).parent 
 full_path = f'{base_path}/saved_dataset.json/data-00000-of-00001.arrow'
-dataset = load_dataset('arrow', data_files=full_path)
-print(len(dataset))
+dataset = load_dataset('arrow', data_files=full_path, split='train')
+print(dataset.features)
