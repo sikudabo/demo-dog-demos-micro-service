1f19a994c028901db1927d387c154c1cbb44afa7
Simeon Ikudabo <simeonikudabo@Simeons-MBP.lan>
2023-12-07 14:42:54 -0500

    Beginning training on new data.

---
+++ squad_dataset.py
@@ -0,0 +1,9 @@
+from datasets import load_dataset 
+from pathlib import Path 
+
+base_path = Path(__file__).parent
+full_path = f'{base_path}/SQuAD.json'
+squad_dataset = load_dataset('json', data_files={'train': full_path, 'validation': full_path})
+
+print(squad_dataset["validation"][2]["context"])
+print(squad_dataset["validation"][2]["question"])
