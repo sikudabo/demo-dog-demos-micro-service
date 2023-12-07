1172648568e5623ceafe44e5d94e13d7505166ab
Simeon Ikudabo <simeonikudabo@Simeons-MBP.lan>
2023-12-07 14:24:54 -0500

    Update

--- experiment.py
+++ experiment.py
@@ -1,6 +1,3 @@
-Context = 'Architecturally, the school has a Catholic character. Atop the Main Building\'s gold dome is a golden statue of the Virgin Mary. Immediately in front of the Main Building and facing it, is a copper statue of Christ with arms upraised with the legend "Venite Ad Me Omnes". Next to the Main Building is the Basilica of the Sacred Heart. Immediately behind the basilica is the Grotto, a Marian place of prayer and reflection. It is a replica of the grotto at Lourdes, France where the Virgin Mary reputedly appeared to Saint Bernadette Soubirous in 1858. At the end of the main drive (and in a direct line that connects through 3 statues and the Gold Dome), is a simple, modern stone statue of Mary.'
-Question = 'To whom did the Virgin Mary allegedly appear in 1858 in Lourdes France?'
-Answer = {'text': ['Saint Bernadette Soubirous'], 'answer_start': [515]}
+string = 'The quick brown fox jumps over the lazy dog.'
 
-the_index = Context.index('Saint')
-print(the_index)
+print(string.index('the lazy'))
