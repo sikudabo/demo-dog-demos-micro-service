addfd5ffc05a5794aacd9992d69645ae343e3a2d
Simeon Ikudabo <simeonikudabo@Simeons-MBP.lan>
2023-12-08 20:47:19 -0500

    Adding the small_eval_set_example.py file

---
+++ small_eval_set_example.py
@@ -0,0 +1,11 @@
+from transformers import AutoTokenizer 
+from squad_dataset import squad_dataset as raw_datasets
+from preprocess_validation_examples import preprocess_validation_examples
+small_eval_set = raw_datasets["validation"].select(range(32)) # This range needs to be less than or equal to the size of the dataset
+trained_checkpoint = "distilbert-base-cased-distilled-squad"
+tokenizer = AutoTokenizer.from_pretrained(trained_checkpoint)
+eval_set = small_eval_set.map(
+    preprocess_validation_examples,
+    batched=True,
+    remove_columns=raw_datasets["validation"].column_names,
+)
